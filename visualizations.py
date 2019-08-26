d35d7d1d9b1fc294a46c2ae732879150a136cf7d
Will Sundstrom <will.a.sundstrom@gmail.com>
2019-08-26 13:27:23 -0400

    copying over all the updated files

--- visualizations.py
+++ visualizations.py
@@ -1,11 +1,14 @@
 """
-This module is for your final visualization code.
-One visualization per hypothesis question is required.
-A framework for each type of visualization is provided.
+This module contains our vizualization plotting methods and our helper functions
+for visualization.
 """
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+import statistics as stats
+import pylab as pl
+import pandas as pd
 
 # Set specific parameters for the visualizations
 large = 22
@@ -23,92 +26,138 @@ plt.style.use('seaborn-whitegrid')
 sns.set_style("white")
 
 
-def overlapping_density(package=None, input_vars=None, target_vars=None):
+def create_sample_dists(cleaned_data, y_var=None, x_var=None, categories=[], samplesize=30, numsamples=400):
+    np.random.seed(5)
     """
-    Set the characteristics of your overlapping density plot
-    All arguments are set to None purely as a filler right now
+    This is our random sampling function to prep data for vizualisations
+    :param cleaned_data: cleaned dataframe
+    :param y_var: The numeric variable you are comparing
+    :param categories: the categories whose means you are comparing
+    :return: a list of sample distributions
+    """
+    df = cleaned_data
 
-    Function takes package name, input variables(categories), and target variable as input.
-    Returns a figure
+    dflist = []
 
-    Should be able to call this function in later visualization code.
+    for cat in categories:
+        dftemp = df.loc[df[x_var].str.contains(cat)][y_var]
+        sampler = np.random.choice(dftemp, size=(samplesize, numsamples))
+        sample_prop = sampler.mean(axis=0)
+        dflist.append(sample_prop)
 
-    PARAMETERS
+    return dflist
 
-    :param package:        should only take sns or matplotlib as inputs, any other value should throw and error
-    :param input_vars:     should take the x variables/categories you want to plot
-    :param target_vars:    the y variable of your plot, what you are comparing
-    :return:               fig to be enhanced in subsequent visualization functions
+
+def overlapping_density(package=None, input_vars=None, target_vars=None, categories=None, output_image_name=None):
+    """ 
+    This plots our pairs of direct comparisons between categories.
     """
 
     # Set size of figure
     fig = plt.figure(figsize=(16, 10), dpi=80)
+    sns.set(color_codes=True)
 
     # Starter code for figuring out which package to use
     if package == "sns":
-        for variable in input_vars:
-            sns.kdeplot(...)
+        for counter, value in enumerate(input_vars):
+            sns.kdeplot(value, label=categories[counter], shade=True)
+            plt.xlabel('Means', fontsize=med)  # , figure = fig)
+            plt.ylabel('Sample counts', fontsize=med)  # , figure = fig)
+            plt.title('Overlapping mean desnsity',
+                      fontsize=large)  # , figure = fig)
+            plt.xticks(fontsize=med)
+            plt.yticks(fontsize=med)
     elif package == 'matplotlib':
         for variable in input_vars:
-            plt.plot(..., label=None, linewidth=None, color=None, figure=fig)
+            plt.plot(variable, label=None, linewidth=None,
+                     color=None, figure=fig)
 
-    return fig
-
-
-def boxplot_plot(package=None, input_vars=None, target_vars=None):
-    """
-    Same specifications and requirements as overlapping density plot
-
-    Function takes package name, input variables(categories), and target variable as input.
-    Returns a figure
-
-    PARAMETERS
-
-    :param package:        should only take sns or matplotlib as inputs, any other value should throw and error
-    :param input_vars:     should take the x variables/categories you want to plot
-    :param target_vars:    the y variable of your plot, what you are comparing
-    :return:               fig to be enhanced in subsequent visualization functions
-    """
-    plt.figure(figsize=(16, 10), dpi=80)
-
-    pass
-
-
-def visualization_one(target_var=None, input_vars=None, output_image_name=None):
-    """
-    The visualization functions are what is used to create each individual image.
-    The function should be repeatable if not generalizable
-    The function will call either the boxplot or density plot functions you wrote above
-
-    :param target_var:
-    :param input_vars:
-    :param output_image_name: the desired name for the image saved
-    :return: outputs a saved png file and returns a fig object for testing
-    """
-    ###
-    # Main chunk of code here
-    ###
-
-    # Starter code for labeling the image
-    plt.xlabel(None, figure=fig)
-    plt.ylabel(None, figure=fig)
-    plt.title(None, figure=fig)
-    plt.legend()
-
-    # exporting the image to the img folder
     plt.savefig(f'img/{output_image_name}.png', transparent=True, figure=fig)
     return fig
 
 
-# please fully flesh out this function to meet same specifications of visualization one
+def color_plot(arr, categories=None, output_image_name=None):
+    """ 
+    This method plots bars for the average citation rate for each category in question
+    """
+    sns.set(color_codes=True)
+    arr_list = np.asarray(arr).mean(axis=1)
+    arr_lst = np.vstack((arr_list, 1-arr_list))
+    df = pd.DataFrame(arr_lst.T)
+    df.plot(kind='bar', stacked=True)
+    plt.xlabel('Vehicle groups', fontsize=med)
+    plt.ylabel('Citation rates', fontsize=med)
+    plt.title('Ticketed vs Non-Ticketed vehicles')
+    plt.xticks(np.arange(len(categories)), categories, rotation=0)
+    plt.legend(labels=['Ticketed', 'Non-Ticketed'])
 
-def visualization_two(output_image_name):
-    pass
+    plt.savefig(f'img/{output_image_name}.png', transparent=True)
 
 
-def visualization_three(output_image_name):
-    pass
+def visualization_one(cleaned_data):
+    # bar plot of color averages
+    short_cleaned_data = cleaned_data[[
+        'color', 'make', 'model', 'violation_type', 'year', 'ticket']]
+
+    vizualization = create_sample_dists(short_cleaned_data,
+                                        y_var='ticket',
+                                        x_var='color',
+                                        categories=['BLACK', 'WHITE', 'SILVER', 'BLUE', 'RED', 'GRAY'])
+    color_plot(vizualization,
+               categories=['BLACK', 'WHITE', 'SILVER', 'BLUE', 'RED', 'GRAY'],
+               output_image_name='Ticketed_vs_Non-Ticketed')
 
 
-def visualization_four(output_image_name):
-    pass
+def visualization_two(cleaned_data):
+    # white vs red cars comparison
+    wr_chart = create_sample_dists(cleaned_data,
+                                   y_var='ticket',
+                                   x_var='color',
+                                         categories=['WHITE', 'RED'])
+
+    overlapping_density('sns',
+                        input_vars=wr_chart,
+                        categories=['WHITE', 'RED'],
+                        output_image_name='Red vs White')
+
+
+def visualization_three(cleaned_data):
+    # white vs blue cars comparison
+    wr_chart = create_sample_dists(cleaned_data,
+                                   y_var='ticket',
+                                   x_var='color',
+                                         categories=['WHITE', 'BLUE'])
+
+    overlapping_density('sns',
+                        input_vars=wr_chart,
+                        categories=['WHITE', 'BLUE'],
+                        output_image_name='White vs Blue')
+
+
+def visualization_four(cleaned_data):
+    # bar plot of make averages
+    cleaned_data['ticket'] = cleaned_data['violation_type'].apply(
+        lambda x: 1 if x == 'Citation' else 0)
+    short_cleaned_data = cleaned_data[[
+        'color', 'make', 'model', 'violation_type', 'year', 'ticket']]
+
+    vizualization = create_sample_dists(short_cleaned_data,
+                                        y_var='ticket',
+                                        x_var='make',
+                                        categories=['NISS', 'FORD', 'HOND', 'TOY'])
+    color_plot(vizualization,
+               categories=['NISS', 'FORD', 'HOND', 'TOY'],
+               output_image_name='Ticketed_vs_Non-Ticketed')
+
+
+def visualization_five(cleaned_data):
+    # honda vs toyota comparison
+    wr_chart = create_sample_dists(cleaned_data,
+                                   y_var='ticket',
+                                   x_var='make',
+                                         categories=['HOND', 'TOY'])
+
+    overlapping_density('sns',
+                        input_vars=wr_chart,
+                        categories=['HOND', 'TOY'],
+                        output_image_name='Honda vs Toyota')
